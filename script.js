6c648b254017d8088af895ac52c1b1ee09fe133f
朱泽豪 <13815398+zhu-zehao12@user.noreply.gitee.com>
2026-03-29 13:11:53 +0800

    t调整一版

--- script.js
+++ script.js
@@ -26,17 +26,20 @@ function playDingSound() {
         oscillator.connect(gainNode);
         gainNode.connect(audioContext.destination);
         
-        // 设置音效参数
-        oscillator.frequency.setValueAtTime(880, audioContext.currentTime); // A5音符
-        oscillator.frequency.exponentialRampToValueAtTime(440, audioContext.currentTime + 0.1);
+        // 使用方波产生类似铃铛的金属质感
+        oscillator.type = 'square';
         
-        // 设置音量包络
-        gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
-        gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.5);
+        // 频率：1046Hz (C6)，经典的铃铛音调
+        oscillator.frequency.setValueAtTime(1046, audioContext.currentTime);
+        
+        // 音量包络 - 模拟敲击铃铛后的自然衰减
+        const now = audioContext.currentTime;
+        gainNode.gain.setValueAtTime(0.6, now);
+        gainNode.gain.exponentialRampToValueAtTime(0.01, now + 0.6);
         
         // 播放声音
-        oscillator.start(audioContext.currentTime);
-        oscillator.stop(audioContext.currentTime + 0.5);
+        oscillator.start(now);
+        oscillator.stop(now + 0.6);
     }
 }
 
